6e61c1a45e77530348536fd9d10b6f726ca15614
zhengzhang <zhengzhang@ruijie.com.cn>
2022-02-14 16:16:45 +0800

    netty4

---
+++ netty4-demo/netty4-server/src/main/java/server/DemoServerHandler.java
@@ -0,0 +1,25 @@
+package server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.net.Inet4Address;
+
+/**
+ * 服务器业务逻辑
+ */
+public class DemoServerHandler extends SimpleChannelInboundHandler {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Client say : " + msg.toString());
+        //返回客户端消息 - 我已经接收到了你的消息
+        ctx.writeAndFlush("Received your message : " + msg.toString());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("RemoteAddress : " + ctx.channel().remoteAddress() + " active !");
+        ctx.writeAndFlush("连接成功！");
+        super.channelActive(ctx);
+    }
+}
