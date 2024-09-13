8121dd59173c81d4458bbeb1263a733a32b317b6
zhengzhang <zhengzhang@ruijie.com.cn>
2024-09-13 11:07:28 +0800

    jni

--- jni-hello-demo/src/main/java/top/zhost/jni/HelloWorldJNI.java
+++ jni-hello-demo/src/main/java/top/zhost/jni/HelloWorldJNI.java
@@ -5,8 +5,8 @@ public class HelloWorldJNI {
 	public native void helloJNI(String name);
 	
 	public static void main(String[] args) {
-		System.loadLibrary("hellojni");
-		//System.load("C:\\Program Files\\Java\\jdk1.8.0_31\\bin\\hellojni.dll");
+		//System.loadLibrary("hellojni");
+		System.load("G:\\project\\c\\CMAKE\\out\\build\\x64-Debug\\hellojni\\hellojni.dll");
 		HelloWorldJNI demo = new HelloWorldJNI();
 		demo.helloJNI("jni test");
 	}
