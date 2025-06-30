80b6d14071733a421c8c31413abaf484a724603e
zhengzhang <zhengzhang@ruijie.com.cn>
2025-06-30 10:08:02 +0800

    file

---
+++ file-string-find/src/main/java/demo/FileStringFind.java
@@ -0,0 +1,11 @@
+package demo;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.concurrent.TimeUnit;
+
+public class FileStringFind {
+    public static void main(String[] args) {
+        System.out.println("FileStringFind");
+    }
+}
