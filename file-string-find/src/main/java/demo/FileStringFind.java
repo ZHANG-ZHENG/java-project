5b5a5d4899f0bc19308be0ab0fb18561f478a59b
zhengzhang <zhengzhang@ruijie.com.cn>
2025-07-01 13:46:52 +0800

    yaml

--- file-string-find/src/main/java/demo/FileStringFind.java
+++ file-string-find/src/main/java/demo/FileStringFind.java
@@ -3,21 +3,13 @@ package demo;
 import demo.bean.FileBean;
 import demo.util.ExcelUtil;
 import demo.util.FileUtil;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
+import demo.util.YamlUtil;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class FileStringFind {
     public static void main(String[] args) {
-        System.out.println("FileStringFind");
-
         String folderPath = "F:\\workspace\\workspace-security-cloud290\\ui\\src\\views\\soc";
         List<String> filesPath = FileUtil.getAllFiles(folderPath);
         //files.forEach(System.out::println);
@@ -46,6 +38,11 @@ public class FileStringFind {
                 }
             }
         }
+
+        // 翻译
+        Map<String, Object> zhYamlMap = YamlUtil.getYamlMap("F:\\workspace\\workspace-security-cloud290\\ui\\locales\\zh-CN.yaml");
+        Map<String, Object> enYamlMap = YamlUtil.getYamlMap("F:\\workspace\\workspace-security-cloud290\\ui\\locales\\en.yaml");
+        System.out.println("1111111" + YamlUtil.getVal(zhYamlMap, "buttons.hsLoginOut"));
 
         String savePath = "F:\\output.xlsx";
         ExcelUtil.exportToExcel(savePath, fileBeanMap, fromFileBeanMap);
---
+++ file-string-find/src/main/java/demo/util/YamlUtil.java
@@ -0,0 +1,40 @@
+package demo.util;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Map;
+
+public class YamlUtil {
+
+    public static Map<String, Object> getYamlMap(String filePath) {
+        Yaml yaml = new Yaml();
+        File file = new File(filePath);
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Map<String, Object> yamlMap = yaml.load(inputStream);
+        return yamlMap;
+    }
+
+    public static String getVal(Map<String, Object> data, String keysString) {
+        String[] keys = keysString.split("\\.");
+        Object current = data;
+        for (String key : keys) {
+            if (current instanceof Map) {
+                current = ((Map<?, ?>) current).get(key);
+            } else {
+                current = null;
+                break;
+            }
+        }
+        return current.toString();
+    }
+
+}
